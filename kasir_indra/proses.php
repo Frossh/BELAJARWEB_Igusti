c91681144478042263fa3e24a1e2729d04d23121
Frossh Indra <mnsgusti@gmail.com>
2023-03-15 22:42:14 +0700

    Update proses.php

--- kasir_indra/proses.php
+++ kasir_indra/proses.php
@@ -1,5 +1,4 @@
 <?php
-
 $jumlah_item1 = $_POST['jumlah_item1'];
 $jumlah_item2 = $_POST['jumlah_item2'];
 $jumlah_item3 = $_POST['jumlah_item3'];
@@ -10,6 +9,7 @@ $total_harga_item2 = $jumlah_item2 * 2500000;
 $total_harga_item3 = $jumlah_item3 * 200000;
 $total_harga_item4 = $jumlah_item4 * 220000;
 $total_harga = $total_harga_item1 + $total_harga_item2;
+echo "<br><H3> Total Harga </H3>";
 
-echo "Total harga: Rp" . number_format($total_harga, 0, ",", ".");
+echo "    Rp." . number_format($total_harga, 0, ",", ".");
 ?>
